a59d0b9de249b1337c26bc021da885e4d727c6a0
lucasdav <lucassouzadavanso@gmail.com>
2024-03-25 17:50:08 -0300

    a lib fs

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-import chalk from "chalk";
+import fs from 'fs';
+import chalk from 'chalk';
 
-console.log(chalk.blue('olá mundo'));
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8'
+    // abaixo usado o underline _ para desconsiderar o parametro
+    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+        console.log(chalk.green(texto))
+    })
+}
+
+pegaArquivo('./arquivos/texto.md');
