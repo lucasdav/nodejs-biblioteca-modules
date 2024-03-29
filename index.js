77bf3194a9f68b2fb32da8aae20e4b47efb0721a
lucasdav <lucassouzadavanso@gmail.com>
2024-03-29 10:23:31 -0300

    capturando grupos

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
+
+function extraiLinks(texto) {
+    // abaixo ogm significa g-global e m-multilinha
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = regex.exec(texto);
+    console.log(capturas);
+}
+
+extraiLinks(textoTeste);
+
 function trataErro(erro) {
     console.log(erro)
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
@@ -33,7 +44,3 @@ async function pegaArquivo(caminhoDoArquivo) {
 
 
 pegaArquivo('./arquivos/texto.md');
-
-// \[[^[\]]*?\]
-
-// \(https?:\/\/[^\s?#.].[^\s]*\)
