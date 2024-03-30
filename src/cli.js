0e3b53f723ee4907a70bd3581e9304ae22c0d097
lucasdav <lucassouzadavanso@gmail.com>
2024-03-30 15:31:43 -0300

    tratando novos erros

--- src/cli.js
+++ src/cli.js
@@ -5,12 +5,24 @@ import pegaArquivo from "./index.js";
 // abaixo executando o comando node src/cli.js ./arquivos/texto.md o process.argv pega o caminho passado
 const caminho = process.argv;
 
-function imprimeLista(resultado) {
-    console.log(chalk.yellow('lista de links'), resultado);
+function imprimeLista(resultado, identificador = '') {
+    console.log(
+        chalk.yellow('lista de links'),
+        chalk.black.bgGreen(identificador),
+        resultado);
 }
 
 async function processaTexto(argumentos) {
     const caminho = argumentos[2];
+
+    try {
+        fs.lstatSync(caminho);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.log('arquivo ou diretório não existe');
+            return;
+        }
+    }
 
     if (fs.lstatSync(caminho).isFile()) {
         const resultado = await pegaArquivo(caminho);
@@ -19,7 +31,7 @@ async function processaTexto(argumentos) {
         const arquivos = await fs.promises.readdir(caminho)
         arquivos.forEach(async (nomeDeArquivo) => {
             const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
-            imprimeLista(lista)
+            imprimeLista(lista, nomeDeArquivo)
         })
     }
 }
