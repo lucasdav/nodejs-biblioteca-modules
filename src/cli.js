44632397391b35464b462a84b64fb6bc5fc9e459
lucasdav <lucassouzadavanso@gmail.com>
2024-03-30 11:56:32 -0300

    processando diretorios

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,27 @@
 import chalk from "chalk";
+import fs from 'fs';
 import pegaArquivo from "./index.js";
 
-// executando o comando node src/cli.js ./arquivos/texto.md o process.argv pega o caminho passado
+// abaixo executando o comando node src/cli.js ./arquivos/texto.md o process.argv pega o caminho passado
 const caminho = process.argv;
 
-async function processaTexto(caminho) {
-    const resultado = await pegaArquivo(caminho[2]);
-    console.log(chalk.yellow('lista de links ', resultado));
+function imprimeLista(resultado) {
+    console.log(chalk.yellow('lista de links'), resultado);
+}
+
+async function processaTexto(argumentos) {
+    const caminho = argumentos[2];
+
+    if (fs.lstatSync(caminho).isFile()) {
+        const resultado = await pegaArquivo(caminho);
+        imprimeLista(resultado)
+    } else if (fs.lstatSync(caminho).isDirectory()) {
+        const arquivos = await fs.promises.readdir(caminho)
+        arquivos.forEach(async (nomeDeArquivo) => {
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
+            imprimeLista(lista)
+        })
+    }
 }
 
 processaTexto(caminho);
