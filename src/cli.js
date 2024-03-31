40a10bb0a64ff2ea7c7c2761a81247f0e4f678f6
lucasdav <lucassouzadavanso@gmail.com>
2024-03-31 12:59:45 -0300

    adicionando opcoes

--- src/cli.js
+++ src/cli.js
@@ -1,19 +1,33 @@
 import chalk from "chalk";
 import fs from 'fs';
 import pegaArquivo from "./index.js";
+import listaValidada from "./http-validacao.js";
 
 // abaixo executando o comando node src/cli.js ./arquivos/texto.md o process.argv pega o caminho passado
 const caminho = process.argv;
 
-function imprimeLista(resultado, identificador = '') {
-    console.log(
-        chalk.yellow('lista de links'),
-        chalk.black.bgGreen(identificador),
-        resultado);
+function imprimeLista(valida, resultado, identificador = '') {
+
+    if (valida) {
+        console.log(
+            chalk.yellow('lista validada'),
+            chalk.black.bgGreen(identificador),
+            listaValidada(resultado));
+    } else {
+        console.log(
+            chalk.yellow('lista de links'),
+            chalk.black.bgGreen(identificador),
+            resultado);
+    }
+
+
+
 }
 
 async function processaTexto(argumentos) {
     const caminho = argumentos[2];
+    // abaixo é executado no comando npm run cli:valida
+    const valida = argumentos[3] === '--valida';
 
     try {
         fs.lstatSync(caminho);
@@ -26,12 +40,12 @@ async function processaTexto(argumentos) {
 
     if (fs.lstatSync(caminho).isFile()) {
         const resultado = await pegaArquivo(caminho);
-        imprimeLista(resultado)
+        imprimeLista(valida, resultado)
     } else if (fs.lstatSync(caminho).isDirectory()) {
         const arquivos = await fs.promises.readdir(caminho)
         arquivos.forEach(async (nomeDeArquivo) => {
             const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
-            imprimeLista(lista, nomeDeArquivo)
+            imprimeLista(valida, lista, nomeDeArquivo)
         })
     }
 }
